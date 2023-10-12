3d3390fcfd85ef678841d46cc79dbd870ce547dc
collinsuc <collins.ahuchaogu@gmail.com>
2023-10-12 21:43:59 +0000

    alx backend.st

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- a SQL script that lists all bands, ranked by their longevity
+
+SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
